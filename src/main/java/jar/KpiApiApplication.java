2b81ca8c22725b14ad2cbe703c00c4dc4cf7bc6f
伊良部　律漬喜 <sekaowa.ritsuki@gmail.com>
2025-12-18 07:16:35 +0900

    Initial commit: Spring Boot project setup

---
+++ src/main/java/jar/KpiApiApplication.java
@@ -0,0 +1,13 @@
+package jar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpiApiApplication.class, args);
+	}
+
+}
